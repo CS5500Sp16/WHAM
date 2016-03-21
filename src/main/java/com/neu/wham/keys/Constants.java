c29b753e140135dc12c0512178373dd185d4b22b
VIJET BADIGANNAVAR <bvijet@gmail.com>
2016-03-21 19:03:41 -0400

    File upload module integrated

---
+++ src/main/java/com/neu/wham/keys/Constants.java
@@ -0,0 +1,5 @@
+package com.neu.wham.keys;
+
+public class Constants {
+	public static final String IMAGE_UPLOAD_PATH = "/usr/uploaded_images/";
+}	
--- src/main/java/com/neu/wham/services/EventServiceImpl.java
+++ src/main/java/com/neu/wham/services/EventServiceImpl.java
@@ -1,16 +1,22 @@
 package com.neu.wham.services;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
 import com.neu.wham.dao.EventDAO;
 import com.neu.wham.exceptions.InvalidAddressException;
+import com.neu.wham.keys.Constants;
 import com.neu.wham.keys.Keys;
 import com.neu.wham.messages.EventPostResponse;
 import com.neu.wham.messages.EventPostResponse.Status;
@@ -24,18 +30,26 @@ public class EventServiceImpl implements NewEventService {
 	@Autowired
 	private EventPostResponse evntResponse;
 	
+	private final Date tempDate = new Date();
+	
 	@Override
-	public EventPostResponse submitNewEvent(Event event) {
+	public EventPostResponse submitNewEvent(Event event, MultipartFile imageFile) {
 		//fetch the lat-lng
 		try {
 			getLatLongForEventLocation(event);
-			Date tempDate = new Date();
+			//save the file if uploaded if uploaded in the request.
+			event.setFilePath(saveUploadedFile(imageFile, event.getOrganiserName()));
 			event.setCreationTime(tempDate);
 			event.setLastUpdateTime(tempDate);
 			eventDAO.addNewEvent(event);
 			evntResponse.setMsg(Status.OK.name());
 			evntResponse.setStatus(Status.OK);
-		} catch (InvalidAddressException e) {
+		}catch (IOException e) {
+			evntResponse.setMsg(e.getMessage());
+			evntResponse.setStatus(Status.ERROR);
+			e.printStackTrace(); // TODO remove this
+		}
+		catch (InvalidAddressException e) {
 			evntResponse.setMsg(e.getMessage());
 			evntResponse.setStatus(Status.ERROR);
 			e.printStackTrace(); // TODO remove this
@@ -45,6 +59,22 @@ public class EventServiceImpl implements NewEventService {
 			e.printStackTrace(); // TODO remove this
 		}
 		return evntResponse;
+	}
+
+	private String saveUploadedFile(MultipartFile imageFile, String organiserName) throws IOException {
+		if(imageFile != null){
+			StringBuilder builder = new StringBuilder();
+			builder.append(Constants.IMAGE_UPLOAD_PATH);
+			builder.append(organiserName);
+			builder.append("_");
+			builder.append(tempDate.getTime());
+			builder.append("_");
+			builder.append(imageFile.getOriginalFilename());
+			File tempFile = new File(builder.toString());
+			FileUtils.writeByteArrayToFile(tempFile, imageFile.getBytes());
+			return builder.toString();
+		}
+		return null;
 	}
 
 	private void getLatLongForEventLocation(Event event) throws InvalidAddressException{
