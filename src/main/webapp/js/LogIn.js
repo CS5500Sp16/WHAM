d4fd10b7f93e469d76533447b6c4bd49db4eb5e9
neha6490 <neha6490@ccs.neu.edu>
2016-03-24 15:50:23 -0400

    changes

--- src/main/webapp/js/LogIn.js
+++ src/main/webapp/js/LogIn.js
@@ -5,16 +5,16 @@ $(document).ready(function () {
 		if ($('#loginForm').valid()) {
 			var neu_email_id = document.getElementById("neu_emailid");
 			var password = document.getElementById("password");
-			var string = neu_email_id.value;
-			var string1 = string.split("@");
 			console.log(string1);
 			if ((neu_email_id.value == "student@husky.neu.edu" || neu_email_id.value == "faculty@neu.edu") && password.value == "secret") {
-			    localStorage.setItem("name",string1[0]);
+			    var string = neu_email_id.value;
+			    var string1 = string.split("@");
+			    localStorage.setItem("name", string1[0]);
 			    return;
 			} else {
 				event.preventDefault();
-				document.getElementById("password").value ="";
-				document.getElementById("neu_emailid").value ="";
+				//document.getElementById("password").value ="";
+				//document.getElementById("neu_emailid").value ="";
 				$("#error").show();
 			}
 		}
