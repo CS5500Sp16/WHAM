b0cdc92c01eb82b2e6a2b655a17cdf305bc03b22
Xi Wang <emmawangxi03@gmail.com>
2016-04-07 22:11:33 -0400

    Add comments to backend 2 tests

--- src/main/java/com/neu/wham/services/PreferenceServiceImpl.java
+++ src/main/java/com/neu/wham/services/PreferenceServiceImpl.java
@@ -42,7 +42,7 @@ public class PreferenceServiceImpl implements PreferenceService {
 		try{
 			return preferenceDAO.getUserPreferences(Integer.valueOf(userId));
 		}catch(Exception e){
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
 		return null;
 	}
